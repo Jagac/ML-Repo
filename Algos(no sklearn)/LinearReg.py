a6e004a0f664939a9d7c8f825fbfcab0ecec0be5
Jagac <jagacnoob2@gmail.com>
2022-01-13 00:47:50 -0600

    Update LinearReg.py

--- Algos(no sklearn)/LinearReg.py
+++ Algos(no sklearn)/LinearReg.py
@@ -14,6 +14,13 @@ class LinearRegression:
         self.bias = 0
 
         for _ in range(self.n_iters):
-            
+            y_predicted = np.dot(X,self.weights) + bias
+            dw = (1/n_samples) * np.dot(X.T,(y_predicted-y))
+            db = (1/n_samples) * np.sum(y_predicted-y)
+
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
 
     def predict(self, X):
+        y_predicted = np.dot(X,self.weights) + bias
+        return y_predicted
