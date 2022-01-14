5520b36b976ed855f061a9317dc0ea466af4242c
Jagac <jagacnoob2@gmail.com>
2022-01-13 20:58:39 -0600

    using time series split

--- Stock Price Prediction(LSTM)/script.py
+++ Stock Price Prediction(LSTM)/script.py
@@ -4,8 +4,21 @@ import matplotlib. pyplot as plt
 from keras.layers import *
 from keras.models import *
 import keras.backend as K
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import TimeSeriesSplit
+
 
 df=pd.read_csv('MSFT.csv',na_values=['null'],index_col='Date',parse_dates=True,infer_datetime_format=True)
 print(df.shape)
+
+output_var = pd.DataFrame(df['Adj Close'])
+features = ['Open', 'High', 'Low', 'Volume']
+
+
+scaler = MinMaxScaler()
+feature_transform = scaler.fit_transform(df[features])
+feature_transform= pd.DataFrame(columns=features, data=feature_transform, index=df.index)
+
+
 
 
