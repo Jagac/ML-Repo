cbe32346255a3aac449ee29114db52c651109a7e
Jagac <jagacnoob2@gmail.com>
2022-01-13 21:19:39 -0600

    updated

--- Stock Price Prediction(LSTM)/script.py
+++ Stock Price Prediction(LSTM)/script.py
@@ -7,18 +7,38 @@ import keras.backend as K
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import TimeSeriesSplit
 
-
 df=pd.read_csv('MSFT.csv',na_values=['null'],index_col='Date',parse_dates=True,infer_datetime_format=True)
 print(df.shape)
 
 output_var = pd.DataFrame(df['Adj Close'])
 features = ['Open', 'High', 'Low', 'Volume']
 
-
 scaler = MinMaxScaler()
 feature_transform = scaler.fit_transform(df[features])
 feature_transform= pd.DataFrame(columns=features, data=feature_transform, index=df.index)
 
+timesplit = TimeSeriesSplit(n_splits=10)
+for train_index, test_index in timesplit.split(feature_transform):
+        X_train, X_test = feature_transform[:len(train_index)], feature_transform[len(train_index): (len(train_index)+len(test_index))]
+        y_train, y_test = output_var[:len(train_index)].values.ravel(), output_var[len(train_index): (len(train_index)+len(test_index))].values.ravel()
 
+trainX =np.array(X_train)
+testX =np.array(X_test)
+X_train = trainX.reshape(X_train.shape[0], 1, X_train.shape[1])
+X_test = testX.reshape(X_test.shape[0], 1, X_test.shape[1])
 
+lstm = Sequential()
+lstm.add(LSTM(32, input_shape=(1, trainX.shape[1]), activation='relu', return_sequences=False))
+lstm.add(Dense(1))
+lstm.compile(loss='mean_squared_error', optimizer='adam')
 
+lstm.fit(X_train, y_train, epochs=100, batch_size=32)
+
+y_pred= lstm.predict(X_test)
+plt.plot(y_test, label='True Value')
+plt.plot(y_pred, label='LSTM Value')
+plt.title('Prediction')
+plt.xlabel('Time Scale')
+plt.ylabel('Scaled USD')
+plt.legend()
+plt.show()
